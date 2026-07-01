9716a1117289d368d3a5bc5bdf45fea49e82f2e6
CSE093 <mohanapriya.m2023cse@sece.ac.in>
2026-07-01 13:56:10 +0530

    Add Question_2

---
+++ Question_2/Main.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter student name: ");
+        String name = sc.nextLine();
+
+        System.out.print("Enter number of subjects: ");
+        int n = sc.nextInt();
+
+        int[] marks = new int[n];
+
+        System.out.println("Enter marks:");
+        for (int i = 0; i < n; i++) {
+            marks[i] = sc.nextInt();
+        }
+
+        Student s = new Student(name, marks);
+
+        System.out.println("Average = " + s.calculateAverage());
+
+        sc.close();
+    }
+}
