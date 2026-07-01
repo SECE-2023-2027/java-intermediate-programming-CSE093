05dea0d19c722b64fd670d908f9517b775535eda
CSE093 <mohanapriya.m2023cse@sece.ac.in>
2026-07-01 14:04:20 +0530

    Add Question_3

---
+++ Question_3/BankAccount.java
@@ -0,0 +1,31 @@
+public class BankAccount {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName, double balance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = balance;
+    }
+
+    public void deposit(double amount) {
+        if (amount <= 0) {
+            System.out.println("Invalid Deposit Amount");
+        } else {
+            balance += amount;
+        }
+    }
+
+    public void withdraw(double amount) {
+        if (amount > balance) {
+            System.out.println("Insufficient Funds");
+        } else {
+            balance -= amount;
+        }
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+}
